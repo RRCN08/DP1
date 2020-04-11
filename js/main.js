8ce16c19ac99442fe52f280b87f4317de30a1429
RRCN08 <43510157+RRCN08@users.noreply.github.com>
2020-04-12 00:50:09 +0530

    kokrajhar updated

--- js/main.js
+++ js/main.js
@@ -1,16 +1,47 @@
 $(document).ready(function() {
 
+      var audio = new Audio("./src/audio/31_Kokrajhar_gossaigaon.wav");
+      var isPlaying = false;
+
     $('#Kokrajhar').on('click',function(){
         $("#district").modal();
    });
 
-     var icon = $('.play');
-     
-    icon.click(function() {
-       icon.toggleClass('active');
-       return false;
+       $('body').on('click', '.btn', function(e){
+    e.preventDefault();
+
+    if ( $(this).hasClass('play') ) {
+      togglePlay();
+      $(this).removeClass('play');
+      $(this).addClass('pause');
+    } else {
+      togglePlay();
+      $(this).removeClass('pause');
+      $(this).addClass('play');
+    }
+
     });
 
+    function togglePlay() {
+      if (isPlaying) {
+        audio.pause();
+        audio.currentTime = 0;
+      } else {
+        audio.play();
+      }
+    };
+
+    audio.onplaying = function() {
+      isPlaying = true;
+    };
+    audio.onpause = function() {
+      isPlaying = false;
+    };
+
+    audio.addEventListener("ended",  function() {
+        isPlaying = false;
+          $('.btn').addClass('play').removeClass('pause');
+      });
 
 
 });
