0e6dd04394caf3c9e8aa8d9294941c6ffc66da74
RRCN08 <43510157+RRCN08@users.noreply.github.com>
2020-04-16 01:01:10 +0530

    district_Update

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
         mymodal.find('.modal-title').text('Kokrajhar');
 
         $(".Player1").attr("style", "display: inline-block;");
-        $(".Player1").attr("id", "kokrajhar_gossaigaon");
+        $(".Player1").attr("id", "kokrajhar_gossaigaon"); 
 
         mymodal.modal('show');
         });
@@ -35,6 +35,92 @@ $(document).ready(function() {
 
         mymodal.modal('show');
         });
+
+        $('#Chirang').on('click',function(){
+          var mymodal = $('#district');
+          $(".districtmap").attr("src", "./css/district/Chirang.svg");
+          mymodal.find('.modal-title').text('Chirang');
+
+          $(".Player1").attr("style", "display: inline-block;");
+          $(".Player1").attr("id", "Chirang_basugaon");
+
+          $(".Player2").attr("style", "display: inline-block;");
+          $(".Player2").attr("id", "chirang_bijni");
+
+          mymodal.modal('show');
+          });
+
+          $('#Goalpara').on('click',function(){
+            var mymodal = $('#district');
+            $(".districtmap").attr("src", "./css/district/Goalpara.svg");
+            mymodal.find('.modal-title').text('Goalpara');
+
+            $(".Player1").attr("style", "display: inline-block;");
+            $(".Player1").attr("id", "Goalpara_lakhipur");
+
+            mymodal.modal('show');
+            });
+
+            $('#Barpeta').on('click',function(){
+              var mymodal = $('#district');
+              $(".districtmap").attr("src", "./css/district/Barpeta.svg");
+              mymodal.find('.modal-title').text('Barpeta');
+
+              $(".Player1").attr("style", "display: inline-block;");
+              $(".Player1").attr("id", "barpeta_howly");
+
+              $(".Player2").attr("style", "display: inline-block;");
+              $(".Player2").attr("id", "barpeta_sarthebari");
+
+              $(".Player3").attr("style", "display: inline-block;");
+              $(".Player3").attr("id", "barpeta_sorbhog");
+
+              $(".Player4").attr("style", "display: inline-block;");
+              $(".Player4").attr("id", "barpeta");
+
+              mymodal.modal('show');
+              });
+
+              $('#Nalbari').on('click',function(){
+                var mymodal = $('#district');
+                $(".districtmap").attr("src", "./css/district/Nalbari.svg");
+                mymodal.find('.modal-title').text('Nalbari');
+
+                $(".Player1").attr("style", "display: inline-block;");
+                $(".Player1").attr("id", "Nalbari");
+
+                $(".Player2").attr("style", "display: inline-block;");
+                $(".Player2").attr("id", "Nalbari_tihu");
+
+                mymodal.modal('show');
+                });
+
+
+                $('#Kamrup').on('click',function(){
+                  var mymodal = $('#district');
+                  $(".districtmap").attr("src", "./css/district/Kamrup.svg");
+                  mymodal.find('.modal-title').text('Kamrup');
+
+                  $(".Player1").attr("style", "display: inline-block;");
+                  $(".Player1").attr("id", "kamrup_rangia");
+
+                  $(".Player2").attr("style", "display: inline-block;");
+                  $(".Player2").attr("id", "Kamrup_pathsala");
+
+                  $(".Player3").attr("style", "display: inline-block;");
+                  $(".Player3").attr("id", "kamrup_palasbari");
+
+                  $(".Player4").attr("style", "display: inline-block;");
+                  $(".Player4").attr("id", "kamrup_guwahati");
+
+                  $(".Player5").attr("style", "display: inline-block;");
+                  $(".Player5").attr("id", "Kamrup_dharapur");
+
+                  mymodal.modal('show');
+                  });
+
+
+
 
 
 
